4d70d2695428b4f8a24dd7ec411f2afa14f55d88
rodjean015 <rodjeanverzosa001@gmail.com>
2026-01-22 13:46:14 +0800

    add android

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,10 @@ import {
   setupIonicReact,
 } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-import { ellipse, scanOutline, homeSharp } from "ionicons/icons";
+import { scanOutline, homeSharp, personSharp } from "ionicons/icons";
 import { useState } from "react";
 
 import Tab1 from "./pages/Tab1";
-import Tab2 from "./pages/Tab2";
 import Tab3 from "./pages/Tab3";
 
 import "@ionic/react/css/core.css";
@@ -37,7 +36,6 @@ const App: React.FC = () => {
         <IonTabs>
           <IonRouterOutlet>
             <Route exact path="/tab1" component={Tab1} />
-            <Route exact path="/tab2" component={Tab2} />
             <Route exact path="/tab3" component={Tab3} />
             <Route exact path="/">
               <Redirect to="/tab1" />
@@ -46,7 +44,7 @@ const App: React.FC = () => {
 
           <IonTabBar slot="bottom" className="custom-tab-bar">
             <IonTabButton tab="tab1" href="/tab1">
-              <IonIcon icon={homeSharp} />
+              <IonIcon icon={homeSharp} size="large" />
             </IonTabButton>
 
             {/* CENTER QR BUTTON */}
@@ -63,8 +61,7 @@ const App: React.FC = () => {
             </IonTabButton>
 
             <IonTabButton tab="tab3" href="/tab3">
-              <IonIcon icon={ellipse} />
-              <IonLabel>Tab 3</IonLabel>
+              <IonIcon icon={personSharp} className="custom-tab-bar" />
             </IonTabButton>
           </IonTabBar>
         </IonTabs>
